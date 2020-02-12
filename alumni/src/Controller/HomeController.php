d68aa65a99f149af90cc21fa3caea1b1ee4aa5c0
serigne20 <dwrbassirou@gmail.com>
2020-02-12 09:10:36 +0100

    connexion

--- alumni/src/Controller/HomeController.php
+++ alumni/src/Controller/HomeController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/jbhbjbjgbh", name="home")
      */
     public function index()
     {
