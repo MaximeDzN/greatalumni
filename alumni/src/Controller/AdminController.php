436aef15e0f30b79a3a2cede58bd82b8bf47569f
Flav1-ann <60137816+Flav1-ann@users.noreply.github.com>
2020-03-02 09:20:53 +0100

    annuaire

--- alumni/src/Controller/AdminController.php
+++ alumni/src/Controller/AdminController.php
@@ -7,8 +7,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\News;
-use App\Form\NewType;
-use App\Form\NewsEditType;
+
 use App\Repository\CommentRepository;
 use App\Repository\NewsRepository;
 
@@ -20,72 +19,13 @@ class AdminController extends AbstractController
     public function index(EntityManagerInterface $manager, Request $request)
     {
      
-        $user = $this->getUser();
-         //On Créer un nouveau User.
-         $news = new News();
-         //On créer le formulaire d'inscription prédéfini dans Form/NewType
-         $newsForm = $this->createForm(NewType::class, $news);
-         //on récupère les données entrées.
-         $newsForm->handleRequest($request);
-         //On vérifie le contenu du formulaire
-         if ($newsForm->isSubmitted() && $newsForm->isValid()) {
-             
-            $news->setDate(new \DateTime('now'));
-                $file = $newsForm->get('media')->getData();      
-                //On créer un nom unique pour l'image   
-                $filename = md5(uniqid()).'.'.$file->guessExtension();
-                //On déplace le fichier vers "avatar_directory"
-                $file->move($this->getParameter('newsImg_directory'),$filename);
-                //On applique la nouvelle photo pour l'utilisateur
-                $news->setMedia($filename);
-                $news->setAuthor($user);
-             $manager->persist($news);
-             $manager->flush();
-             return $this->redirectToRoute('news');
-         }
+       
          return $this->render('admin/index.html.twig', [
-            'newsForm' => $newsForm->createView(),
-        ]); 
-    }
-
-      /**
-     * @Route("/admin/news_edit/{id}", name="news_edit")
-     */
-    public function newsedit(EntityManagerInterface $manager, Request $request,News $news )
-    {
-        $user = $this->getUser();
-        $media = $news->getMedia();
-        if($user == $news->getAuthor() || in_array('ROLE_ADMIN', $user->getRoles()) ){
-        $newsForm = $this->createForm(NewsEditType::class, $news);
-         //on récupère les données entrées.
-         $newsForm->handleRequest($request);
-         //On vérifie le contenu du formulaire
-         if ($newsForm->isSubmitted() && $newsForm->isValid()) {
-            if ($news->getMedia() != null  ){
             
-                $file = $newsForm->get('media')->getData();      
-                //On créer un nom unique pour l'image   
-                $filename = md5(uniqid()).'.'.$file->guessExtension();
-                //On déplace le fichier vers "avatar_directory"
-                $file->move($this->getParameter('newsImg_directory'),$filename);
-                //On applique la nouvelle photo pour l'utilisateur
-                $news->setMedia($filename);
-            } else {
-                $news->setMedia($media);
-            }
-             $manager->persist($news);
-             $manager->flush();
-             return $this->redirectToRoute('news');
-         }
-        } else {
-            return $this->redirectToRoute('app_login');
-        }
-        return $this->render('news/edit.html.twig', [
-            'news' => $news,
-            'newsForm' => $newsForm->createView(),
         ]); 
     }
 
+     
 
        /**
      * @Route("/admin/news_del/{id}", name="news_del")
--- alumni/src/Controller/DirectoryController.php
+++ alumni/src/Controller/DirectoryController.php
@@ -2,25 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\PropertySearch;
 use App\Entity\User;
+use App\Form\PropertySearchType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
+use PhpParser\Node\Name;
 
 class DirectoryController extends AbstractController
 {
     /**
      * @Route("/directory", name="directory")
      */
-    public function index()
+    public function index(Request $request)
     {
         $repo = $this->getDoctrine()->getRepository(User::class);
 
         $users = $repo->findAll();
 
-
+        $search = new PropertySearch();
+        $form = $this->createForm(PropertySearchType::class, $search);
+        $form->handleRequest($request);
+      
         return $this->render('directory/index.html.twig', [
             'controller_name' => 'DirectoryController',
-            'users' => $users
+            'users' => $users,
+            'formSearch' => $form->createView(),
         ]);
     }
 
@@ -35,4 +44,6 @@ class DirectoryController extends AbstractController
             'user' => $user
         ]);
     }
+
+    
 }
